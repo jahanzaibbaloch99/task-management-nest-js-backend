f7f4bbd46b5ad11a369cd700b55fcb4be1cae3ec
Jahanzaib Ali <jhanzeb.ali99@gmail.com>
2021-12-17 01:51:52 +0500

    init controllers and methods

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
 
@@ -6,7 +6,12 @@ import { TasksService } from './tasks.service';
 export class TasksController {
     constructor(private tasksService: TasksService) { }
     @Get()
-    getAllTasks() :Task[] {
+    getAllTasks(): Task[] {
         return this.tasksService.getAllTasks()
+    }
+
+    @Post()
+    createTask(@Body("title") title, @Body('description') description): Task {
+        return this.tasksService.createTask(title, description)
     }
 }

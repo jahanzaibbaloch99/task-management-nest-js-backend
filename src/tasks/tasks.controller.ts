96a74ca20a9dbb78df208c1e7f9fa0e2b1ad03de
Jahanzaib Ali <jhanzeb.ali99@gmail.com>
2021-12-16 23:37:40 +0500

    model added

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Task } from './tasks.model';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    constructor(private tasksService: TasksService) { }
+    @Get()
+    getAllTasks() :Task[] {
+        return this.tasksService.getAllTasks()
+    }
+}

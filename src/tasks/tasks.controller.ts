a3c6f0ed5043e3a49ffcc0fcfea6cb0ac06eead3
Jahanzaib Ali <jhanzeb.ali99@gmail.com>
2021-12-17 21:09:10 +0500

    dto & get task by id added

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
 
@@ -10,8 +11,13 @@ export class TasksController {
         return this.tasksService.getAllTasks()
     }
 
+    @Get("/:id")
+    getTaskById(@Param("id") id: string) {
+        return this.tasksService.getTaskById(id)
+    }
+
     @Post()
-    createTask(@Body("title") title, @Body('description') description): Task {
-        return this.tasksService.createTask(title, description)
+    createTask(@Body() createTaskDto: CreateTaskDto): Task {
+        return this.tasksService.createTask(createTaskDto)
     }
 }
